b45ce6636332a0b3e94d20bededb38b8bee89fa2
ally4n <94357446+ally4n@users.noreply.github.com>
2022-03-16 22:07:02 +0300

    Update train.py

--- train.py
+++ train.py
@@ -120,12 +120,12 @@ def train(hyp, opt, device, tb_writer=None, wandb=None):
     # plot_lr_scheduler(optimizer, scheduler, epochs)
 
     # Logging
-    if wandb and wandb.run is None:
-        opt.hyp = hyp  # add hyperparameters
-        wandb_run = wandb.init(config=opt, resume="allow",
-                               project='YOLOR' if opt.project == 'runs/train' else Path(opt.project).stem,
-                               name=save_dir.stem,
-                               id=ckpt.get('wandb_id') if 'ckpt' in locals() else None)
+    #if wandb and wandb.run is None:
+    #    opt.hyp = hyp  # add hyperparameters
+    #    wandb_run = wandb.init(config=opt, resume="allow",
+    #                           project='YOLOR' if opt.project == 'runs/train' else Path(opt.project).stem,
+    #                           name=save_dir.stem,
+    #                           id=ckpt.get('wandb_id') if 'ckpt' in locals() else None)
 
     # Resume
     start_epoch, best_fitness = 0, 0.0
